1c7efbae19ef835d4e622dce0ec09082ead895d6
tiffany-ts <tiffany@trunk-studio.com>
2021-04-22 16:15:18 +0000

    update test.js

--- test.js
+++ test.js
@@ -8,7 +8,7 @@ http.createServer(function(request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
 
    // Send the response body "Hello World"
-   response.end('Hello World!\n');
+   response.end('Hello World Again!\n');
 }).listen(8000);
 
 // Print URL for accessing server
